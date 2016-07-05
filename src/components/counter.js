5ec48934c23d7b39aee60b915f815d662d1f07e9
Vitalii Kozii <koziy.vitaliy@gmail.com>
2016-07-05 12:37:25 +0300

    added actions +10 and -10

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,19 +4,25 @@ import { connect } from 'react-redux';
 import {
   incrementAction,
   decrementAction,
+  increment10Action,
+  decrement10Action,
 } from '../actions';
 
 const Counter = (props) => (
   <div className={styles.counter}>
+    <button onClick={props.doIncrement10}>+10</button>
+    <button onClick={props.doIncrement} className={styles.mrl10}>+</button>
     <h1>{props.counter}</h1>
-    <button onClick={props.doIncrement}>+</button>
-    <button onClick={props.doDecrement}>-</button>
+    <button onClick={props.doDecrement} className={styles.mrl10}>-</button>
+    <button onClick={props.doDecrement10} className={styles.mrl10}>-10</button>
   </div>
 );
 Counter.propTypes = {
   counter: PropTypes.number,
   doIncrement: PropTypes.func,
   doDecrement: PropTypes.func,
+  doIncrement10: PropTypes.func,
+  doDecrement10: PropTypes.func,
 };
 
 const mapStateToProps = (state) => ({ counter: state.counter });
@@ -27,6 +33,12 @@ const mapDispatchToProps = (dispatch) => ({
   },
   doDecrement: () => {
     dispatch(decrementAction());
+  },
+  doIncrement10: () => {
+    dispatch(increment10Action());
+  },
+  doDecrement10: () => {
+    dispatch(decrement10Action());
   },
 });
 
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,6 +1,8 @@
 import {
   INCREMENT,
   DECREMENT,
+  INCREMENT_10,
+  DECREMENT_10,
 } from '../actionTypes';
 import initialState from '../initialstate';
 
@@ -10,6 +12,10 @@ export const CounterReducer = (state = initialState, action) => {
       return state + 1;
     case DECREMENT:
       return state - 1;
+    case INCREMENT_10:
+      return state + 10;
+    case DECREMENT_10:
+      return state - 10;
     default:
       return initialState;
   }
