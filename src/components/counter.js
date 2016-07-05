33cd8c1b76f5872783d299d4e9331c871750ba70
Vitalii Kozii <koziy.vitaliy@gmail.com>
2016-07-05 13:17:13 +0300

    moved out dispatch function

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import styles from '../../static/css/app.css';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import {
   incrementAction,
   decrementAction,
@@ -10,36 +11,28 @@ import {
 
 const Counter = (props) => (
   <div className={styles.counter}>
-    <button onClick={props.doIncrement10}>+10</button>
-    <button onClick={props.doIncrement} className={styles.mrl10}>+</button>
+    <button onClick={props.increment10Action}>+10</button>
+    <button onClick={props.incrementAction} className={styles.mrl10}>+</button>
     <h1>{props.counter}</h1>
-    <button onClick={props.doDecrement} className={styles.mrl10}>-</button>
-    <button onClick={props.doDecrement10} className={styles.mrl10}>-10</button>
+    <button onClick={props.decrementAction} className={styles.mrl10}>-</button>
+    <button onClick={props.decrement10Action} className={styles.mrl10}>-10</button>
   </div>
 );
 Counter.propTypes = {
   counter: PropTypes.number,
-  doIncrement: PropTypes.func,
-  doDecrement: PropTypes.func,
-  doIncrement10: PropTypes.func,
-  doDecrement10: PropTypes.func,
+  incrementAction: PropTypes.func,
+  decrementAction: PropTypes.func,
+  increment10Action: PropTypes.func,
+  decrement10Action: PropTypes.func,
 };
 
-const mapStateToProps = (state) => ({ counter: state.counter });
-
-const mapDispatchToProps = (dispatch) => ({
-  doIncrement: () => {
-    dispatch(incrementAction());
-  },
-  doDecrement: () => {
-    dispatch(decrementAction());
-  },
-  doIncrement10: () => {
-    dispatch(increment10Action());
-  },
-  doDecrement10: () => {
-    dispatch(decrement10Action());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(
+  state => ({ counter: state.counter }),
+  dispatch => bindActionCreators(
+    {
+      incrementAction,
+      decrementAction,
+      increment10Action,
+      decrement10Action,
+    }, dispatch)
+)(Counter);
