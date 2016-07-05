079df98d8466190cc0fab795abd14d605f453e88
Vitalii Kozii <koziy.vitaliy@gmail.com>
2016-07-05 12:17:05 +0300

    done logger

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -7,7 +7,7 @@ const DevTools = createDevTools(
   <DockMonitor
     toggleVisibilityKey="ctrl-h"
     changePositionKey="ctrl-q"
-    defaultIsVisible={Boolean(true)}
+    defaultIsVisible={Boolean(false)}
   >
     <LogMonitor theme="tomorrow" />
   </DockMonitor>
--- src/store.js
+++ src/store.js
@@ -2,22 +2,29 @@
  This file defines the main Redux Store. It will be required by all "smart" components in the app
  */
 
-import { combineReducers, createStore, compose } from 'redux';
+import { applyMiddleware, combineReducers, createStore, compose } from 'redux';
 import { CounterReducer } from './reducers/counter';
 import { persistState } from 'redux-devtools';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import DevTools from './containers/DevTools';
 
-const rootReducer = combineReducers({
-  counter: CounterReducer,
-});
 function getDebugSessionKey() {
   // You can write custom logic here!
   // By default we try to read the key from ?debug_session=<key> in the address bar
   const matches = window.location.href.match(/[?&]debug_session=([^&#]+)\b/);
   return (matches && matches.length > 0) ? matches[1] : null;
 }
+const logger = createLogger();
 const enhancer = compose(
+  applyMiddleware(thunk, promise, logger),
   DevTools.instrument(),
   persistState(getDebugSessionKey())
 );
-export default createStore(rootReducer, {}, enhancer);
+
+const rootReducer = combineReducers({
+  counter: CounterReducer,
+});
+
+export default createStore(rootReducer, enhancer);
