67a84f4c086389e022fcab60e50d950b651dee1b
Vitalii Kozii <koziy.vitaliy@gmail.com>
2016-07-12 11:06:25 +0300

    set router

--- src/components/content.js
+++ src/components/content.js
@@ -1,6 +1,18 @@
+//  import modules
 import React from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+//  import components
 import Home from './home';
+import { NotFound } from './notFound';
 
+/**
+ * Please keep routes in alphabetical order
+ */
 export const Content = () => (
-  <Home />
+  <Router history={browserHistory}>
+    <Route path="/" component={Home}>
+      <IndexRoute component={Home} />
+      <Route path="*" component={NotFound} />
+    </Route>
+  </Router>
 );
