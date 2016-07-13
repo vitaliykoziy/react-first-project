ce836277cdba9673ca3f1f2d2b62b8fc103990ff
Vitalii Kozii <koziy.vitaliy@gmail.com>
2016-07-13 11:58:14 +0300

    done validtion of login and signup forms

--- src/components/signUp/signUpForm.js
+++ src/components/signUp/signUpForm.js
@@ -1,10 +1,37 @@
 //  import modules
 import React, { PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
+import FormMessages, { generateValidation } from 'redux-form-validation';
 //  import styles
 import styles from './signUp.css';
 
 export const formName = 'signUp';
+
+const validations = {
+  email: {
+    validateOnBlur: true,
+    required: true,
+    minLength: 5,
+    email: true,
+  },
+  password: {
+    required: true,
+    minLength: 6,
+  },
+  comparePassword: {
+    required: true,
+    matchField: 'password',
+  },
+  firstName: {
+    required: true,
+    minLength: 6,
+  },
+  lastName: {
+    required: true,
+    minLength: 6,
+  },
+
+};
 
 const SignUpForm = ({
     fields: {
@@ -19,6 +46,17 @@ const SignUpForm = ({
   <form onSubmit={handleSubmit} className={styles.signUpForm}>
     <h1>Sign Up to MyBusiness</h1>
     <div className={styles.inputGroup}>
+      <FormMessages tagName="ol" className={styles.hasError} errorCount="2" field={email}>
+        <li when="required">
+          this field is required
+        </li>
+        <li when="email">
+          please insert a valid email
+        </li>
+        <li when="minLength">
+          this field must have at least 5 characters
+        </li>
+      </FormMessages>
       <label>
         <span>Enter Your email</span>
         <input
@@ -29,6 +67,14 @@ const SignUpForm = ({
       </label>
     </div>
     <div className={styles.inputGroup}>
+      <FormMessages tagName="ol" className={styles.hasError} errorCount="2" field={password}>
+        <li when="required">
+          this field is required
+        </li>
+        <li when="minLength">
+          this field must have at least 6 characters
+        </li>
+      </FormMessages>
       <label>
         <span>Enter Your password</span>
         <input
@@ -39,6 +85,14 @@ const SignUpForm = ({
       </label>
     </div>
     <div className={styles.inputGroup}>
+      <FormMessages tagName="ol" className={styles.hasError} errorCount="2" field={comparePassword}>
+        <li when="required">
+          this field is required
+        </li>
+        <li when="matchField">
+          is different than password, should be same
+        </li>
+      </FormMessages>
       <label>
         <span>Enter Your password again</span>
         <input
@@ -49,6 +103,14 @@ const SignUpForm = ({
       </label>
     </div>
     <div className={styles.inputGroup}>
+      <FormMessages tagName="ol" className={styles.hasError} errorCount="2" field={firstName}>
+        <li when="required">
+          this field is required
+        </li>
+        <li when="minLength">
+          this field must have at least 6 characters
+        </li>
+      </FormMessages>
       <label>
         <span>Enter Your First name</span>
         <input
@@ -59,6 +121,14 @@ const SignUpForm = ({
       </label>
     </div>
     <div className={styles.inputGroup}>
+      <FormMessages tagName="ol" className={styles.hasError} errorCount="2" field={lastName}>
+        <li when="required">
+          this field is required
+        </li>
+        <li when="minLength">
+          this field must have at least 6 characters
+        </li>
+      </FormMessages>
       <label>
         <span>Enter Your Last name</span>
         <input
@@ -79,11 +149,5 @@ SignUpForm.propTypes = {
 };
 export default reduxForm({
   form: formName,
-  fields: [
-    'password',
-    'comparePassword',
-    'email',
-    'firstName',
-    'lastName',
-  ],
+  ...generateValidation(validations),
 })(SignUpForm);
